53410b3352f6ee967c8f587bd3ca3c653b855e18
VanLy672 <ly.tran23@student.passerellesnumeriques.org>
2023-02-09 20:19:09 +0700

    feat/auth/register

--- src/screens/Register.jsx
+++ src/screens/Register.jsx
@@ -5,16 +5,66 @@ import {
   View,
   TouchableOpacity,
   TextInput,
+  Alert,
 } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import RegisterStyles from '../styles/RegisterStyles';
 import IconAntDesign from 'react-native-vector-icons/AntDesign';
 import IconFeather from 'react-native-vector-icons/Feather';
 import IconFontisto from 'react-native-vector-icons/Fontisto';
-import {useNavigation} from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
+import axios from 'axios';
 
 const Register = () => {
+  
   const navigation = useNavigation();
+
+  const [fullName, setFullName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleRegister = async () => {
+    if (password !== confirmPassword) {
+      Alert.alert('Error', 'Passwords do not match');
+      return;
+    }
+
+    try {
+      const response = await axios.post(
+        'http://ec2-13-250-122-122.ap-southeast-1.compute.amazonaws.com/api/register',
+        {
+          fullname: fullName,
+          phonenumber: phoneNumber,
+          email,
+          password,
+          password_confirmation: confirmPassword,
+        },
+      );
+
+      const data = response.data;
+      if (data.error) {
+        Alert.alert('Error', data.error);
+      } else {
+        navigation.navigate('Login');
+        Alert.alert('Success', 'Registration successful');
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 422) {
+        const {errors} = error.response.data;
+        let errorMessage = 'Validation errors: ';
+        for (const field in errors) {
+          errorMessage += `${field} ${errors[field].join(', ')}, `;
+        }
+        Alert.alert('Error', errorMessage);
+      } else {
+        console.error(error);
+        Alert.alert('Error', 'Something went wrong, please try again');
+      }
+    }
+  };
+
   return (
     <View style={RegisterStyles.container}>
       <View style={RegisterStyles.image}>
@@ -31,6 +81,8 @@ const Register = () => {
         <TextInput
           style={RegisterStyles.textInput}
           placeholder="What’s your name?"
+          value={fullName}
+          onChangeText={text => setFullName(text)}
         />
       </View>
       <View style={RegisterStyles.formInput}>
@@ -43,6 +95,8 @@ const Register = () => {
         <TextInput
           style={RegisterStyles.textInput}
           placeholder="What’s your phone number?"
+          value={phoneNumber}
+          onChangeText={text => setPhoneNumber(text)}
         />
       </View>
       <View style={RegisterStyles.formInput}>
@@ -55,6 +109,8 @@ const Register = () => {
         <TextInput
           style={RegisterStyles.textInput}
           placeholder="What’s your email?"
+          value={email}
+          onChangeText={text => setEmail(text)}
         />
       </View>
       <View style={RegisterStyles.formInput}>
@@ -67,6 +123,9 @@ const Register = () => {
         <TextInput
           style={RegisterStyles.textInput}
           placeholder="Enter your password..."
+          secureTextEntry
+          value={password}
+          onChangeText={text => setPassword(text)}
         />
       </View>
       <View style={RegisterStyles.formInput}>
@@ -79,9 +138,12 @@ const Register = () => {
         <TextInput
           style={RegisterStyles.textInput}
           placeholder="Confirm your password..."
+          secureTextEntry
+          value={confirmPassword}
+          onChangeText={text => setConfirmPassword(text)}
         />
       </View>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={handleRegister}>
         <Text style={RegisterStyles.btnRegister}>
           Register
           <IconAntDesign name="arrowright" size={16} color="#ffffff" />
