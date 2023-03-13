5969d68618b61676a12f48808329ccc86cf0a67c
VanLy672 <ly.tran23@student.passerellesnumeriques.org>
2023-03-13 15:37:11 +0700

    update style component Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -22,6 +22,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     padding: 10,
     marginVertical: 10,
+    marginHorizontal: 10,
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
