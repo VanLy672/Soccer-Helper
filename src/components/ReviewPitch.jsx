516083e2940dd47ec7c14adfdfcd0ef3227c0d1f
VanLy672 <ly.tran23@student.passerellesnumeriques.org>
2023-03-13 15:37:37 +0700

    change interface ReviewPitch

--- src/components/ReviewPitch.jsx
+++ src/components/ReviewPitch.jsx
@@ -3,14 +3,8 @@ import {View, Image, Text} from 'react-native';
 const ReviewPitch = ({name, avatar, title}) => {
   return (
     <View>
-      <Text>{name}</Text>
-      <Image
-        source={{
-          uri: avatar,
-        }}
-        style={{width: 150, height: 100}}
-      />
-      <Text>{title}</Text>
+      <Text style={{fontWeight: 'bold', fontSize: 17}}>{name}:</Text>
+      <Text style={{marginBottom: 10}}>{title}</Text>
     </View>
   );
 };
