e4b0e18527ebbef0f5ddcfe410c066bf079a44aa
VanLy672 <ly.tran23@student.passerellesnumeriques.org>
2023-03-15 08:58:24 +0700

    update UI when the post has no image.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,18 +2,35 @@ import React from 'react';
 import {View, Text, Image, StyleSheet} from 'react-native';
 
 const Post = ({avatar, username, content, image}) => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.userInfo}>
-        <Image source={{uri: avatar}} style={styles.avatar} />
-        <Text style={styles.username}>{username}</Text>
+  if (image === 'http://ec2-13-250-122-122.ap-southeast-1.compute.amazonaws.com/') {
+    return (
+      <View style={styles.container}>
+        <View style={styles.userInfo}>
+          <Image source={{uri: avatar}} style={styles.avatar} />
+          <Text style={styles.username}>{username}</Text>
+        </View>
+        <View style={styles.content}>
+          <Text>{content}</Text>
+        </View>
       </View>
-      <View style={styles.content}>
-        <Text>{content}</Text>
-        {image && <Image source={{uri: image}} style={styles.image} />}
+    );
+  }
+    return (
+      <View style={styles.container}>
+        <View style={styles.userInfo}>
+          <Image source={{uri: avatar}} style={styles.avatar} />
+          <Text style={styles.username}>{username}</Text>
+        </View>
+        <View style={styles.content}>
+          <Text>{content}</Text>
+          {image && (
+            <View style={styles.imageContainer}>
+              <Image source={{uri: image}} style={styles.image} />
+            </View>
+          )}
+        </View>
       </View>
-    </View>
-  );
+    );
 };
 
 const styles = StyleSheet.create({
@@ -53,6 +70,10 @@ const styles = StyleSheet.create({
     height: 200,
     marginTop: 10,
     borderRadius: 10,
+  },
+  imageContainer: {
+    marginTop: 10,
+    marginBottom: 10,
   },
 });
 
